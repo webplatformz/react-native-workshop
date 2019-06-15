b9429739d5c7a166b9cc50b4042d5f943015784c
Dominic Mülhaupt <dominic.muelhaupt@zuehlke.com>
2019-06-15 15:44:05 +0200

    Add exercises

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Button, Dimensions, Image, StyleSheet, TextInput, View } from 'react-native';
-
-let giveAnswer = () => {
-    alert(`Uhmmm... well... maybe!`)
-}
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <TextInput
-                style={styles.questionBox}
-                placeholder='Ask me...'
-            />
-            <Button
-                title='I want to know 🔮'
-                onPress={giveAnswer}
-            />
+            {/*
+                1. Add <TextInput /> (https://facebook.github.io/react-native/docs/textinput), don't forget to import
+                2. Add style property
+                ...
+                4. Add placeholder property
+            */}
+            {/*
+                5. Add <Button /> with title (https://facebook.github.io/react-native/docs/button)
+                6. Hint: Button component doesn't support style property, you can add marginBottom to questionBox
+                ...
+                8. Add onPress property
+            */}
             <Image
                 style={styles.image}
                 source={{uri: 'http://www.prognostik.com/wp-content/uploads/2015/10/prognostik_orakel_delphi_w.jpg'}}
@@ -24,22 +24,14 @@ export default function App() {
     );
 }
 
+// 7. Define function const giveAnswer = () => {} (Hint: Use alert())
+
 const styles = StyleSheet.create({
     container: {
         backgroundColor: 'lightblue',
         alignItems: 'center',
     },
-    questionBox: {
-        backgroundColor: 'white',
-        marginTop: 50,
-        width: Dimensions.get('window').width - 40,
-        height: 50,
-
-        padding: 10,
-        fontSize: 18,
-
-        marginBottom: 20,
-    },
+    // 3. Add style for questionBox (Hints: backgroundColor, marginTop, width, height, padding, fontSize)
     image: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
